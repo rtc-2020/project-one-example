d057c6dcd0be75e7d02da5f5bfcf4ae17ad0adc8
Karl Stolley <karl.stolley@gmail.com>
2020-09-30 12:20:41 -0500

    Move file-watching logic to own lib module

--- app.js
+++ app.js
@@ -11,11 +11,14 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const io = require('socket.io')();
 const util = require('./lib/utilities');
+const watch = require('./lib/watch');
 
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 
 const app = express();
+
+var fileEvent = new EventEmitter();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,43 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
-var old_headlines = JSON.parse(fs.readFileSync('var/abc.old.json', {encoding:"utf8"}));
-
-var fileEvent = new EventEmitter();
-
-fs.watch('var/abc.html', function(eventType, filename) {
-  fs.promises.readFile(`var/${filename}`, {encoding:"utf8"})
-    .then(function(data) {
-      var new_file = data;
-      // Use Cheerio to pull headlines of interest
-      let qs = cheerio.load(new_file);
-      let headlines = [];
-      qs('.headlines-ul li a').each(function(i,elem){
-        var data = qs(elem);
-        var headline = {};
-        headline.text = data.text().trim();
-        headline.url = data.attr('href').trim();
-        headlines.push(headline);
-      });
-
-      // Compare the old headlines with the new; return the new ones
-      let new_headlines = util.findUniqueObjects(headlines,old_headlines,'url');
-
-      // Only proceed if there are new headlines to work with
-      if (new_headlines.length > 0) {
-        // Process headlines into an HTML payload
-        let html_headlines = new_headlines.map(function(hl) {
-          return `<li><a href="${hl.url}">${hl.text}</a></li>`;
-        });
-
-        fileEvent.emit('new headlines', html_headlines);
-        old_headlines = headlines;
-        // Write latest headlines to files
-        fs.promises.writeFile('var/abc.latest.json', JSON.stringify(new_headlines), {encoding:"utf8"});
-        fs.promises.writeFile('var/abc.old.json', JSON.stringify(headlines), {encoding:"utf8"});
-      }
-    });
-  });
+watch.headlines(fileEvent);
 
 // send a message on successful socket connection
 io.on('connection', function(socket){
